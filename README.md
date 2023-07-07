ecfdf49b0ea598b5f4beb06270097488754546d7
junhee <wjunhee102@gmail.com>
2023-07-07 22:31:22 +0900

    init

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # study-wjddnd
+
+웅이 계산기 만들기
---
+++ index.js
@@ -0,0 +1,30 @@
+const leftInput = document.getElementById("left");
+const centerInput = document.getElementById("center");
+const rightInput = document.getElementById("right");
+const resultDom = document.getElementById("result");
+const actionButton = document.getElementById("action");
+
+/**
+ * a, b의 숫자 값과 operator를 이용하여 계산기를 만드시오.
+ * 기본 적으로 +, -, -, / 가 동작해야함.
+ * 결과값을 return 만 해주면 됨.
+ */
+function calculate(a, b, operator) {
+  if (operator === "+") {
+    return a + b;
+  }
+
+  return 0;
+}
+
+function renderResult() {
+  const a = leftInput.value;
+  const b = rightInput.value;
+  const operator = centerInput.value;
+
+  const result = calculate(a, b, operator);
+
+  resultDom.innerHTML = `${result}`;
+}
+
+actionButton.addEventListener("click", renderResult);
